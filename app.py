710eb94b1f72a72c394880231df5e6fb0fc7ef11
Tianchi Liu <tliu240@bloomberg.net>
2019-01-20 01:10:56 +0000

    cannot redirect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template, flash, redirect, url_for, request
 from redis import Redis, RedisError
 import os
 import socket
@@ -7,20 +7,50 @@ import socket
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
-@app.route("/")
-def hello():
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+@app.route("/", methods=['GET', 'POST'])
+def login():
     try:
         visits = redis.incr("counter")
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    user = {'username': 'Tianchi Liu'}
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=user['username'], hostname=socket.gethostname(), visits=visits)
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/test")
+def test():
+  return "Hello word"
+
+@app.route("/index")
+def index():
+  posts = [
+      {
+          'author': {'username': 'John'},
+          'body': 'Beautiful day in Portland!'
+      },
+      {
+          'author': {'username': 'Susan'},
+          'body': 'The Avengers movie was so cool!'
+      }
+  ]
+  return render_template('index.html', title='Home', user=request.args['user'], posts=posts)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
