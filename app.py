d10ad4d921aeb28caa3c1f72d3d3e3bcb54a90ba
Tianchi Liu <tliu240@bloomberg.net>
2019-01-20 08:17:27 +0000

    base.html works

--- app.py
+++ app.py
@@ -28,9 +28,7 @@ def login():
 
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
+      return redirect(url_for('index', user=form.username.data))
 
     return render_template('login.html', title='Login', form=form)
 
